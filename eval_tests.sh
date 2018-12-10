f88da190b8c85339ef205d77405bafef14029c68
Nick Hawke <n.hawke96@gmail.com>
2018-12-10 15:14:13 -0500

    finalize testing
    - recompile forkbomb_v5
    - fix eval_tests.sh vigenere path
    - switch analyze_binary.py to use exact edit distance not approximation

--- eval_tests.sh
+++ eval_tests.sh
@@ -4,7 +4,7 @@
 trap 'exit' SIGINT
 
 
-progs=(forkbomb/forkbomb_v1 forkbomb/forkbomb_v2 forkbomb/forkbomb_v3 forkbomb/forkbomb_v4 forkbomb/forkbomb_v5 forkbomb/benign_while mario/mario resize/resize resize/vigenere)
+progs=(forkbomb/forkbomb_v1 forkbomb/forkbomb_v2 forkbomb/forkbomb_v3 forkbomb/forkbomb_v4 forkbomb/forkbomb_v5 forkbomb/benign_while mario/mario resize/resize vigenere/vigenere)
 n=${#progs[@]}
 
 RES=res.out
@@ -22,7 +22,7 @@ do
         file2=${progs[j]}
         echo "${file1} ${file2}" | tee -a $RES
         #timeout --foreground 30 ./analyze_binary.py -g tests/$file1 tests/$file2 | tee /dev/tty | tail -n1 >> $RES
-        timeout --foreground 30 ./analyze_binary.py -g tests/$file1 tests/$file2 | tail -n1 | tee -a $RES
+        timeout --foreground 30s ./analyze_binary.py -g tests/$file1 tests/$file2 | tail -n1 | tee -a $RES
         echo "" | tee -a  $RES
     done;
 done 2> /dev/null
