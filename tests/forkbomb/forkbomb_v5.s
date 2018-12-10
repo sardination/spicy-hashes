4688049cfafa740680c54e4e8ed2405868846371
ameguid123 <adham_meguid@college.harvard.edu>
2018-12-10 14:32:30 -0500

    Update forkbomb_v5 for 32-bit

--- tests/forkbomb/forkbomb_v5.s
+++ tests/forkbomb/forkbomb_v5.s
@@ -5,47 +5,47 @@
 _main:                                  ## @main
 	.cfi_startproc
 ## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset %ebp, -8
+	movl	%esp, %ebp
+	movl	%esp, %ebp
+	.cfi_def_cfa_register %ebp
+	subl	$24, %esp
+	movl	$0, -4(%ebp)
 	## InlineAsm Start
 	nop
 	## InlineAsm End
-	movl	$42, -8(%rbp)
+	movl	$42, -8(%ebp)
 	## InlineAsm Start
 	nop
 	## InlineAsm End
-	movl	$0, -12(%rbp)
+	movl	$0, -12(%ebp)
 LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
-	cmpl	$10, -12(%rbp)
+	cmpl	$10, -12(%ebp)
 	jge	LBB0_4
 ## %bb.2:                               ##   in Loop: Header=BB0_1 Depth=1
 	## InlineAsm Start
 	nop
 	## InlineAsm End
-	movl	-12(%rbp), %eax
-	addl	-8(%rbp), %eax
-	movl	%eax, -8(%rbp)
+	movl	-12(%ebp), %eax
+	addl	-8(%ebp), %eax
+	movl	%eax, -8(%ebp)
 	## InlineAsm Start
 	nop
 	## InlineAsm End
 ## %bb.3:                               ##   in Loop: Header=BB0_1 Depth=1
-	movl	-12(%rbp), %eax
+	movl	-12(%ebp), %eax
 	subl	$-1, %eax
-	movl	%eax, -12(%rbp)
+	movl	%eax, -12(%ebp)
 	jmp	LBB0_1
 LBB0_4:
 	## InlineAsm Start
 	nop
 	## InlineAsm End
 LBB0_5:                                 ## =>This Inner Loop Header: Depth=1
-	callq	_fork
-	movl	%eax, -16(%rbp)         ## 4-byte Spill
+	calll	_fork
+	movl	%eax, -16(%ebp)         ## 4-byte Spill
 	jmp	LBB0_5
 	.cfi_endproc
                                         ## -- End function
